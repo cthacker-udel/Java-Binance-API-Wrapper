84718f64d62cba1c139ec54c43d086fdb9cf85c7
cthacker-udel <cthacker@udel.edu>
2021-05-12 05:14:50 -0400

    Update - Added General Endpoint Test File

--- src/test/java/GeneralEndpointTest.java
+++ src/test/java/GeneralEndpointTest.java
@@ -1,18 +1,33 @@
 import Client.BinanceClient;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GeneralEndpointTest {
 
+    static BinanceClient client;
+
     static {
-
-        BinanceClient client = new BinanceClient();
-
+        client = new BinanceClient();
     }
 
 
     @Test
-    void testConnectivityTest(){
+    void testConnectivityTest() throws IOException {
+        assertTrue(client.testConnectivity(client));
+    }
 
+    @Test
+    void testExchangeInformation() throws IOException {
+        assertNotNull(client.getExchangeInformation(client));
+    }
+
+    @Test
+    void testServerTime() throws IOException {
+        assertNotNull(client.getServerTime(client));
     }
 
 }
